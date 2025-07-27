0e8a5dfc91db2603674fdb1ad2ca5cf459f10d3b
HansTtito <kvttitos@gmail.com>
2025-07-27 16:59:28 -0400

    install packages.R added

---
+++ install-packages.R
@@ -0,0 +1,17 @@
+packages <- c(
+  "rsconnect",
+  "Tivy",
+  "dplyr",
+  "leaflet",
+  "pdftools",
+  "png",
+  "raster"
+)
+
+new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if (length(new_packages)) {
+  install.packages(new_packages, dependencies = TRUE)
+  cat("✅ Paquetes instalados:", paste(new_packages, collapse = ", "), "\n")
+} else {
+  cat("✅ Todos los paquetes ya están instalados\n")
+}
